c11caafd5a0381bd26b82dbbd37aa34a230ed533
450Oucema <450oucema@gmail.com>
2020-06-17 21:42:40 +0200

    fix fixtures

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -5,6 +5,7 @@ namespace App\DataFixtures;
 use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Ramsey\Uuid\Uuid;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class UserFixtures extends Fixture
@@ -22,6 +23,9 @@ class UserFixtures extends Fixture
         $user->setEmail('oucema45@gmail.com');
         $user->setRoles(["ROLE_ADMIN"]);
         $user->setPassword($this->encoder->encodePassword($user,'oucemahe10'));
+        $user->setPrivate(false);
+        $user->setUuid((Uuid::uuid4())->toString());
+        $user->setPrivateForFriends(false);
         $this->setReference('oucema', $user);
 
         $manager->persist($user);
